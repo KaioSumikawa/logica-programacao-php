41e73e333487684f0d7e9ad077da3a2a9e0b0e70
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-06-06 16:58:22 -0300

    feat: adiciona formulário para cadastro de novo contato

--- aula01/add_contato.php
+++ aula01/add_contato.php
@@ -1,18 +1,19 @@
 <?php include "includes/header.php"; ?>
 
 <h1>Adicionar Contato</h1>
+
 <form action="opcontato.php?acao=cadastrar" method="post">
   <div class="mb-3">
-    <label>Nome</label>
-    <input type="text" name="nome" class="form-control" required>
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome" class="form-control" required>
   </div>
   <div class="mb-3">
-    <label>Email</label>
-    <input type="email" name="email" class="form-control" required>
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email" class="form-control" required>
   </div>
   <div class="mb-3">
-    <label>Telefone</label>
-    <input type="text" name="telefone" class="form-control" required>
+    <label for="telefone">Telefone</label>
+    <input type="text" name="telefone" id="telefone" class="form-control" required>
   </div>
   <button type="submit" class="btn btn-primary">Salvar</button>
   <a href="index.php" class="btn btn-secondary">Voltar</a>
