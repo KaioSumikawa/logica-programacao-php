eaf0be6c0ef15565c61117f50c43fb728b642c43
Kaio Sumikawa <kaiosumikawa@gmail.com>
2025-06-06 16:59:23 -0300

    feat: adiciona página inicial com introdução à Aula 02

--- aula02/index.php
+++ aula02/index.php
@@ -1,6 +1,6 @@
 <?php include "includes/header.php"; ?>
 
 <h1>Bem-vindo à Aula 02 - Lógica de Programação</h1>
-<p>Escolha um exemplo no menu acima para ver os códigos organizados e profissionais.</p>
+<p>Utilize o menu acima para navegar entre exemplos de códigos organizados e profissionais.</p>
 
 <?php include "includes/footer.php"; ?>
